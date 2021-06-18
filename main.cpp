5370aeb7f3f8655e371b104e99d7642294e45f87
Boris Nimcevic <borisnimcevic@gmail.com>
2021-06-18 17:56:21 +0200

    Deleted old class for Single LED.

--- main.cpp
+++ main.cpp
@@ -1,53 +1,24 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
+#include "DisplayDriver.hpp"
 #include "SingleLed.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <unistd.h>
 
 int main() {
 
+  sf::Clock clock;
   SingleLed led;
-  led.setON();
-  std::cout << "Led state: " << led.getState() << "\n";
+  Display display(&led);
 
-/*   // Window: */
-/*   sf::RenderWindow window(sf::VideoMode(200, 200), "7-segment simulator"); */
-/*   window.setFramerateLimit(30); */
-/*   window.clear(); */
+  while (display.isDisplayOpen()) {
+    if (clock.getElapsedTime().asMilliseconds() > led.getDutyCycle()) {
+      clock.restart();
+      led.toggleState();
+    }
 
-/*   SingleLED led1(50.f, 50.f, 100.f, 500, sf::Color::Red); */
-
-/*   // Clock: */
-/*   sf::Clock clock; */
-
-/*   while (window.isOpen()) { */
-/*     sf::Event event; */
-/*     while (window.pollEvent(event)) { */
-/*       switch (event.type) { */
-/*       case sf::Event::Closed: */
-/*         window.close(); */
-/*         break; */
-
-/*       case sf::Event::KeyPressed: */
-/*         if (event.key.code == sf::Keyboard::Escape) */
-/*           window.close(); */
-/*         break; */
-
-/*       default: */
-/*         // Nothing for now. It is just here so compiler doesn't complain about */
-/*         // unhandled events in the switch. */
-/*         break; */
-/*       } */
-/*     } */
-
-/*     if (clock.getElapsedTime().asMilliseconds() > led1.getDutyCycle()) { */
-/*       clock.restart(); */
-/*       led1.toggleLED(); */
-/*     } */
-
-/*     window.clear(); */
-/*     window.draw(*led1.getLED()); */
-/*     window.display(); */
-/*   } */
+    display.update();
+    display.show();
+  }
 
   return EXIT_SUCCESS;
 }
