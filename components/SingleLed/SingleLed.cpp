16cfad464e1b5ba36d5fc30e1d91eb962177947e
Boris Nimcevic <borisnimcevic@gmail.com>
2021-06-18 15:45:36 +0200

    Added new Led library.

---
+++ components/SingleLed/SingleLed.cpp
@@ -0,0 +1,5 @@
+#include "SingleLed.h"
+
+int multiply(int a, int b){
+  return a * b;
+}
---
+++ components/SingleLed/include/SingleLed.h
@@ -0,0 +1,6 @@
+#ifndef _SingleLed_H_
+#define _SingleLed_H_
+
+int multiply(int, int);
+
+#endif //_SingleLed_H_
